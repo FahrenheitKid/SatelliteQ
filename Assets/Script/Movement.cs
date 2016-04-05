cbce187e52a116dac70dce490114a418c4bf9a02
Mateus Ribeiro <mateussilvanh@hotmail.com>
2016-04-05 20:37:53 -0300

    Duct, Ladder e etc. Fix

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -8,19 +8,25 @@ public class Movement : MonoBehaviour {
     public Camera FirstPerson;
     public Camera TopView;
 
-    public float speedWalk = 15.0f;
-    public float speedWalkBackwards = 5.0f;
-    public float speedRun = 28.0f;
-    public float jumpSpeed = 8.0F;
-    public float gravity = 9.8F;
-    private float speed = 30.0f;
-    private bool jumping = false;
+    public float speedWalk = 20.0f;
+    public float speedWalkBackwards = 15.0f;
+    public float speedRun = 40.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 70.0f;
+    private float speed = 20.0f;
+    private bool ladderTrigger = false;
 
     private Vector3 cameraRotation = Vector3.zero;
     public float mouseSpeed = 100.0f;
 
     private Vector3 translation = Vector3.zero;
     private Vector3 rotation = Vector3.zero;
+
+    private Vector3 cameraPos = new Vector3(0, 13.0f, 2);
+    private Vector3 crouchCameraPos = new Vector3(0, 7.5f, 2);
+    private Vector3 crouchCapsuleCenter = new Vector3(0, 3.5f, 0);
+    private Vector3 capsuleCenter = new Vector3(0, 6.25f, 0);
+    private bool onDuct = false;
 
     float cameraRotatedX = 0;
 
@@ -40,7 +46,14 @@ public class Movement : MonoBehaviour {
         rotation.x = Input.GetAxis("Mouse Y") * mouseSpeed * Time.deltaTime;
 
         translation.x = Input.GetAxis("Horizontal");
-        translation.y = 0;
+        if (ladderTrigger == true)
+        {
+            translation.y = Input.GetAxis("Vertical");
+        }
+        else
+        {
+            translation.y = 0;
+        }
         translation.z = Input.GetAxis("Vertical");
         translation = transform.TransformDirection(translation);
         translation *= speed;
@@ -58,16 +71,55 @@ public class Movement : MonoBehaviour {
         FirstPerson.transform.localEulerAngles = cameraRotation;
     }
 
+    void OnTriggerEnter(Collider obj)
+    {
+        if(obj.tag == "Ladder")
+        {
+            ladderTrigger = true;
+            gravity = 0;
+        }
+
+        if (obj.tag == "Duct")
+        {
+            if(!onDuct)
+            {
+                controller.height = 7;
+                controller.center = crouchCapsuleCenter;
+                onDuct = true;
+            }
+            else
+            {
+                controller.height = 12.5f;
+                controller.center = capsuleCenter;
+                onDuct = false;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider obj)
+    {
+        if (obj.tag == "Ladder")
+        {
+            ladderTrigger = false;
+            gravity = 70.0f;
+        }
+
+        if (obj.tag == "Duct")
+        {
+            
+        }
+    }
+
     //Set Animation Flags
     void AnimationControl()
     {
         //Running Jump
         if (controller.isGrounded)
         {
-            if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Space))
+            if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.Space))
             {
                 anim.SetTrigger("isRunningJump");
-                translation.y = 500;
+                translation.y = 1000;
                 Debug.Log("Jumped");
             }
         }
@@ -79,11 +131,17 @@ public class Movement : MonoBehaviour {
         //Crouch
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
+            FirstPerson.transform.localPosition = crouchCameraPos;
+            controller.height = 7;
+            controller.center = crouchCapsuleCenter;
             anim.SetBool("isCrouching", true);
             anim.SetBool("isCrouch", true);
         }
         else if (Input.GetKeyUp(KeyCode.LeftControl))
         {
+            FirstPerson.transform.localPosition = cameraPos;
+            controller.height = 12.5f;
+            controller.center = capsuleCenter;
             anim.SetBool("isCrouchWalking", false);
             anim.SetBool("isCrouching", false);
             anim.SetBool("isCrouch", false);
@@ -118,17 +176,6 @@ public class Movement : MonoBehaviour {
             anim.SetBool("isWalking", false);
         }
 
-        //Walk Backwards
-        if (Input.GetKey(KeyCode.S))
-        {
-            anim.SetBool("isWalkingBackwards", true);
-            //speed = speedWalkBackwards;
-        }
-        else
-        {
-            anim.SetBool("isWalkingBackwards", false);
-        }
-
         //Run
         if (translation.z != 0 && Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
         {
@@ -138,26 +185,6 @@ public class Movement : MonoBehaviour {
         else
         {
             anim.SetBool("isRunning", false);
-        }
-
-        //Run Left Strafe
-        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.LeftShift))
-        {
-            anim.SetBool("isLeftStrafe", true);
-        }
-        else
-        {
-            anim.SetBool("isLeftStrafe", false);
-        }
-
-        //Run Right Strafe
-        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.LeftShift))
-        {
-            anim.SetBool("isRightStrafe", true);
-        }
-        else
-        {
-            anim.SetBool("isRightStrafe", false);
         }
     }
 
