92025ae72446622ac2399d468584cd965a9da47d
Mateus Ribeiro <mateussilvanh@hotmail.com>
2016-06-22 04:31:10 -0300

    Simple Menu

---
+++ Assets/Script/MenuScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour
+{
+    public GUISkin skin;
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    void OnGUI()
+    {
+        GUI.skin = skin;
+
+        if (GUI.Button(new Rect(Screen.width/2 - 100, Screen.height/2 - 15, 200, 30), "Start Game"))
+            Debug.Log("Start Game");
+        if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 30, 200, 30), "Options"))
+            Debug.Log("Options");
+        if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 75, 200, 30), "Quit"))
+            Debug.Log("Quit");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
