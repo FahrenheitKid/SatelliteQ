72e851db15fc2c919116f29e02857c73d9b2a392
Gabriel Shanks Cunegato <milkshanks@gmail.com>
2016-06-23 06:53:17 -0300

    Opening

--- Assets/Script/MenuScript.cs
+++ Assets/Script/MenuScript.cs
@@ -1,28 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.SceneManagement;
 public class MenuScript : MonoBehaviour
 {
+    opening op;
     public GUISkin skin;
 	// Use this for initialization
 	void Start () {
-
+        op = GameObject.Find("op").GetComponent<opening>();
     }
 
     void OnGUI()
     {
         GUI.skin = skin;
 
-        if (GUI.Button(new Rect(Screen.width/2 - 100, Screen.height/2 - 15, 200, 30), "Start Game"))
-            Debug.Log("Start Game");
+        if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 15, 200, 30), "Start Game"))
+        {
+           // SceneManager.LoadScene(1);
+            op.start = true;
+        }
         if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 30, 200, 30), "Options"))
             Debug.Log("Options");
         if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 75, 200, 30), "Quit"))
-            Debug.Log("Quit");
+        {
+            Application.Quit();
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
 	
+        if (op.done)
+        {
+            SceneManager.LoadScene(1);
+        }
 	}
 }
---
+++ Assets/opening.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class opening : MonoBehaviour {
+    Material[] botMaterial1;
+    Material[] botMaterial2;
+    Material[] mountMaterials;
+    float botSlice = 0;
+    float mountSlice = 0;
+   public bool start = false;
+   public bool done;
+	// Use this for initialization
+	void Start () {
+        botMaterial1 = GameObject.Find("Y_Bot/Alpha_Joints").GetComponent<SkinnedMeshRenderer>().materials;
+        botMaterial2 = GameObject.Find("Y_Bot/Alpha_Surface").GetComponent<SkinnedMeshRenderer>().materials;
+        mountMaterials = GameObject.Find("terrain").GetComponent<MeshRenderer>().materials;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (start == true && botSlice < 1.0f)
+        {
+            mountSlice += Time.deltaTime;
+            for (int i = 0; i < mountMaterials.Length; i++)
+            {
+                mountMaterials[i].SetFloat("_SliceAmount", mountSlice);
+            }
+            if (mountSlice > 0.3f)
+            {
+                botSlice += Time.deltaTime;
+                for(int i = 0; i < botMaterial1.Length; i++)
+                {
+                    botMaterial1[i].SetFloat("_SliceAmount", botSlice);
+                    botMaterial2[i].SetFloat("_SliceAmount", botSlice);
+                }
+            }
+            if (botSlice >= 1.0f)
+            {
+                done = true;
+            }
+        }
+	
+	}
+}
